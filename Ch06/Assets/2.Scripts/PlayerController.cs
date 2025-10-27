895f3e80b34bfd9d8db715780de4b01cdf2c468c
jaeugi333-png <jaeugi333@gmail.com>
2025-10-27 17:43:17 +0900

    ch06

---
+++ Ch06/Assets/2.Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpForce = 600f;
+    public float walkForce = 30f;
+    public float maxWalkSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+
+        float speedx = Mathf.Abs(rb.velocity.x);
+
+        if (speedx < maxWalkSpeed)
+        {
+            rb.AddForce(transform.right * key * walkForce);
+        }
+
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+    }
+}
