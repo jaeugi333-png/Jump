5a6c7dc9d3bc0f7f4f011c0ab54c69d8799707e2
jaeugi333-png <jaeugi333@gmail.com>
2025-11-05 10:18:50 +0900

    ch06 완료

--- Ch06/Assets/2.Scripts/PlayerController.cs
+++ Ch06/Assets/2.Scripts/PlayerController.cs
@@ -64,4 +64,20 @@ public class PlayerController : MonoBehaviour
         //Debug.Log("Goal!!");
         SceneManager.LoadScene("ClearScene");
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "cloud")
+        {
+            transform.SetParent(collision.gameObject.transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "cloud")
+        {
+            transform.parent = null;
+        }
+    }
 }
